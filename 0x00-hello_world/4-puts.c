7720c6120cfb139cc62acee7e5a0f16d9bdc4f89
Josephat001 <141118204+Josephat001@users.noreply.github.com>
2023-10-05 19:22:33 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-<<<<<<< HEAD
-	char str[] = "Programming is like building a multilingual puzzle;
-=======
-	char str[] = "Programming is like building a multilingual puzzle
->>>>>>> c83051e8fee04bc15b5d97971927acbb11028c46
-	puts(str);
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
