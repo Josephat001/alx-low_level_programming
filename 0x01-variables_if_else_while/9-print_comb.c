728f9121b1c9cb90c82fb9b7cc733d45b2060ed5
Josephat001 <141118204+Josephat001@users.noreply.github.com>
2023-10-07 03:19:59 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,11 +13,13 @@ int main(void)
 	int num;
 
 	for (num = '0'; num <= '9'; num++)
-		putchar(num);
-	if (num < '9')
 	{
-		putchar(',');
-		putchar(' ');
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 
