fe29865834bbfb769e297e35874586b06e078c08
Josephat001 <141118204+Josephat001@users.noreply.github.com>
2023-10-07 02:07:22 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -12,10 +12,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++) {
+	for (i = 0; i < 10; i++) 
+	{
 		putchar(i + '0');
-		}
-		putchar('\n');
+	}
+	putchar('\n');
 
-		return (0);
+	return (0);
 }
