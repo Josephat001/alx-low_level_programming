dcdc6574b13dc4625d62ab94983c2ef62f35caff
Josephat001 <141118204+Josephat001@users.noreply.github.com>
2023-10-07 15:53:57 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,19 +10,19 @@
 
 int main(void)
 {
-	int i;
-	int j;
+	int a;
+	int b;
 
-	for (i = 0; i <= 99; i++)
+	for (a = 0; a <= 99; a++)
 	{
-		for (j = i + 1; j <= 99; j++)
+		for (b = i + 1; b <= 99; b++)
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
+			putchar(a / 10 + '0');
+			putchar(a % 10 + '0');
 			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
-			if (i < 98)
+			putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
+			if (a < 98)
 			{
 				putchar(',');
 				putchar(' ');
