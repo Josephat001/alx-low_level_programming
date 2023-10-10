0c2eb5ce64eac00061d3a90e1ea2d8a2dc3d458b
josephat001 <jayoyoke@gmail.com>
2023-10-10 16:31:23 -0700

    update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,11 +10,11 @@
 
 void print_alphabet(void)
 {
-    char letter;
+	char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-	   _ putchar(letter);
-    }
-    _putchar('\n');
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
 }
