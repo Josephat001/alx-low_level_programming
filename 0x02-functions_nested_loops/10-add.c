076950246dfb391e181cff958b1d63ee4f26cd72
josephat001 <jayoyoke@gmail.com>
2023-10-11 11:19:09 -0700

    Update 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - check main
+ * @i: an integer input
+ * @j: an integer input
+ * Description: This function prints the sum of two integers
+ * Return: sum of i and j
+ */
+
+int add(int i, int j)
+{
+	int ret = i + j;
+
+	return (ret);
+}
