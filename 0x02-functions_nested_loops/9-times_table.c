4a71bbcbbeb124ffbc31087503ed0b88781636fa
Josephat001 <141118204+Josephat001@users.noreply.github.com>
2023-10-11 23:38:57 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,36 +9,33 @@
 
 void times_table(void)
 {
-	long int i, j;
+    int i, j, result;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			int ret = i * j;
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            result = i * j;
 
-			if (j == 0)
-			{
-				_putchar('0' + 0);
-			}
-				else if (ret < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + ret);
-				}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + (ret / 10));
-				_putchar('0' + (ret % 10));
-				if (j == 9)
-				{
-					_putchar('\n');
-				}
-			}
-		}
-	}
+            if (j == 0)
+            {
+                _putchar('0' + result);
+            }
+            else if (result < 10)
+            {
+                _putchar(',');
+                _putchar(' ');
+                _putchar(' ');
+                _putchar('0' + result);
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+                _putchar('0' + (result / 10));
+                _putchar('0' + (result % 10));
+            }
+        }
+        _putchar('\n');
+    }
 }
