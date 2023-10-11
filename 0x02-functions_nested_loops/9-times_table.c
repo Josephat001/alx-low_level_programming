9fae15c59c91df794576fd5c56309ef79e9d825e
Josephat001 <141118204+Josephat001@users.noreply.github.com>
2023-10-11 17:59:04 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,11 +19,12 @@ void times_table(void)
 
 			if (ret <= 9)
 			{
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(ret + '0');
 				if (j < 9)
 				{
 					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 				}
 			}
