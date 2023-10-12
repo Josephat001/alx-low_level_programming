ab06f6199dd971de213be451a80399c6715c353a
Josephat001 <141118204+Josephat001@users.noreply.github.com>
2023-10-12 17:14:37 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,9 +18,9 @@ void more_numbers(void)
 		{
 			if (b > 9)
 			{
-				_putchar((b / 10) + '0');
+				_putchar((b / 10) + 48);
 			}
-			_putchar((b % 10) + '0');
+			_putchar((b % 10) + 48);
 		}
 		_putchar('\n');
 	}
