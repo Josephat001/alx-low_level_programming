dcc1c86214fade9142b87ea415b8f230ca9794a0
Josephat001 <141118204+Josephat001@users.noreply.github.com>
2023-10-12 17:21:43 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,23 +2,21 @@
 #include "main.h"
 
 /**
- * more_numbers - check main
- * Description: This function prints numbers multiple times
- *
- * Return: Nothing
+ * more_numbers - prints numbers between 0 to 14
+ * 10 times.
+ * Return: no return.
  */
-
 void more_numbers(void)
 {
-	int i, j;
+	int i, ch;
 
-	for (i = '0'; i = 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = '0'; j < 15; j++)
+		for (ch = 0; ch < 15; ch++)
 		{
-			if (j >= 10)
-				_putchar((j / 10) + 48);
-			_putchar((j % 10) + 48);
+			if (ch >= 10)
+				_putchar((ch / 10) + 48);
+			_putchar((ch % 10) + 48);
 		}
 		_putchar('\n');
 	}
