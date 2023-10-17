495cd2df7e73b92faae0f01b6b4d0a0afda4e2eb
josephat001 <jayoyoke@gmail.com>
2023-10-16 17:59:56 -0700

    Update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - check 2-main.c
+ * Description: This function prints the length of the string 's'
+ * @s: the string whose length is to be printed
+ *
+ * Return: Always 0 (Success).
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while
+		(*s != '\0');
+	{
+		len++;
+		s++;
+		return (len);
+
+	}
+}
