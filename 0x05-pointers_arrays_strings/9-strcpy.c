9db630f4d6895510e70bd449847e04f471695f95
Josephat Oyoke <141118204+Josephat001@users.noreply.github.com>
2023-10-18 01:57:12 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,7 +7,7 @@
  * to the buffer pointed to by 'dest'
  * @src: a pointer
  * @dest: a pointer
- * Return: a string of character if successful 
+ * Return: a string of character if successful
  */
 
 char *_strcpy(char *dest, char *src)
@@ -15,15 +15,15 @@ char *_strcpy(char *dest, char *src)
 	int len = 0;
 	int i;
 
-	while (src[len] != '\0' )
+	while (src[len] != '\0')
 	{
 		len++;
 	}
-	
+
 	for (i = 0; i < len ; i++)
-	{ 
+	{
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
 	return (dest);
-} 
+}
