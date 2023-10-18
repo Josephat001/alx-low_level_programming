2289c3baf003b2173b5147a2723fcd7b636d7e63
Josephat Oyoke <141118204+Josephat001@users.noreply.github.com>
2023-10-18 01:55:33 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,18 +12,18 @@
 
 char *_strcpy(char *dest, char *src)
 {
-      int len = 0;
-      int i;
-	
+	int len = 0;
+	int i;
+
 	while (src[len] != '\0' )
 	{
 		len++;
 	}
 	
-	for(i = 0; i < len ; i++)
+	for (i = 0; i < len ; i++)
 	{ 
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
-	return(dest);
+	return (dest);
 } 
