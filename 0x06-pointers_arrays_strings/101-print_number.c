6c876296c388ebd475a1eee09c458481391dba9d
Josephat Oyoke <141118204+Josephat001@users.noreply.github.com>
2023-10-20 00:35:51 +0100

    Update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,18 +11,18 @@
 
 void print_number(int n)
 {
-	unsigned int n;
-
+	unsigned int number;
+	
+	number = n;
 	if (n < 0)
 	{
 		_putchar(45);
-		n = -n;
+		number = -n;
 	}
-
-	if (n / 10)
+	
+	if (number / 10)
 	{
-		print_number(n / 10);
+		print_number(number / 10);
 	}
-
-	_putchar((n % 10) + '0');
+	_putchar((number % 10) + '0');
 }
