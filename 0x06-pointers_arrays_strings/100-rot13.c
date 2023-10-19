703c0fe77a9d330dca4634c82a797cab5ec88fb9
josephat001 <jayoyoke@gmail.com>
2023-10-19 16:12:06 -0700

    Update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,32 +2,32 @@
 #include "main.h"
 
 /**
- * rot13 - This function encodes a string
- *
- * Return: str
+ * rot13 - This function encodes a string using rot13
+ * @s: the pointer to string
+ * Return: the encoded string
  */
 
-char *rot13(char *)
+char *rot13(char *s)
 {
-	int stringCount, rotation;
-	char r1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+	int strC, c;
+	char c1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
 		     'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
 		     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
 		     'Z'};
-	char r2[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+	char c2[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
 		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 		     'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
 		     'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
 		     'M'};
 
-	for (stringCount = 0; s[stringCount] != '\0'; stringCount++)
+	for (strC = 0; s[strC] != '\0'; strC++)
 	{
-		for (rotation = 0; rotation < 53; rotation++)
+		for (c = 0; c < 53; c++)
 		{
-			if (r1[rotation] == s[stringCount])
+			if (c1[c] == s[strC])
 			{
-				s[stringCount] = r2[rotation];
+				s[strC] = c2[c];
 				break;
 			}
 		}
