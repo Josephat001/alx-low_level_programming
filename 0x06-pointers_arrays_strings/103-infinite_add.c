4e412954b3e2830bbe5ac44af6c0745bbe600e4b
Josephat Oyoke <141118204+Josephat001@users.noreply.github.com>
2023-10-20 14:45:28 +0100

    Update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,4 +11,31 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+  int l1 = strlen(n1);
+    int l2 = strlen(n2);
+    int carry = 0;
+    int max_l = l1 > l2 ? l1 : l2;
+    
+    if (max_l + 1 > size_r) {
+        return 0;
+    }
 
+    r[max_l + 1] = '\0';
+
+    while (l1 > 0 || l2 > 0) {
+        int digit1 = (l1 > 0) ? n1[--l1] - '0' : 0;
+        int digit2 = (l2 > 0) ? n2[--l2] - '0' : 0;
+        int sum = digit1 + digit2 + carry;
+        carry = sum / 10;
+        r[max_l--] = (sum % 10) + '0';
+    }
+
+    if (carry) {
+        if (max_l < 0) {
+            return 0;
+        }
+        r[max_l] = carry + '0';
+    }
+
+    return &r[max_l];
+}
