fe37ab32c0ab7e82de90976ebea73b764f27a961
Josephat Oyoke <141118204+Josephat001@users.noreply.github.com>
2023-10-20 15:39:51 +0100

    Update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -15,9 +15,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int a, b, c, d, e, f;
 for (a = 0; n1[a]; a++)
-;
 for (b = 0; n2[b]; b++)
-;
 if (a > size_r || b > size_r)
 return (0);
 e = 0;
