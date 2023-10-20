f8378c5c28927875feaf3662e7ab84db0148f9c9
josephat001 <jayoyoke@gmail.com>
2023-10-20 06:40:05 -0700

    Update

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * infinite_add - check 103-main.c
+ * Description: this function adds two numbers, using buffer and buffer size
+ * @n1: the first number to be added
+ * @n2: the second number to be added
+ * Return: a pointer to the result
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
