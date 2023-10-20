e697bab876dc5cd708bb1cdbc31de87137212e59
josephat001 <jayoyoke@gmail.com>
2023-10-20 08:56:08 -0700

    Update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,14 +10,15 @@
 void print_number(int n)
 {
 	unsigned int num;
+
 	num = n;
-	
+
 	if (n < 0)
 	{
 		_putchar(45);
 		num = -n;
 	}
-	
+
 	if (num / 10)
 	{
 		print_number(num / 10);
--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,31 +2,33 @@
 #include "main.h"
 
 /**
- * print_line - this function prints a buffer,
+ * print_buffer - this function prints a buffer,
  * printing the content of 'size' bytes of the buffer pointed by 'b'
  * @b: buffer to print
  * @size: bytes of buffer to print
- * @line: line of buffer to print
  *
  * Return: void
  */
 
 void print_buffer(char *b, int size)
 {
-int i;
-for (i = 0; i <= (size - 1) / 10 && size; i++)
-{
-printf("%08x: ", i * 10);
-if (i < size / 10)
-{
-print_line(b, 9, i);
-}
-else
-{
-print_line(b, size % 10 - 1, i);
-}
-putchar('\n');
-}
-if (size == 0)
-putchar('\n');
+	int i;
+
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+	{
+		putchar('\n');
+	}
 }
