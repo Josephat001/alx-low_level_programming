0269645de92116465fe03b0c623c4f40cca65321
Josephat Oyoke <141118204+Josephat001@users.noreply.github.com>
2023-10-20 15:09:34 +0100

    Update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,31 +11,36 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-  int l1 = strlen(n1);
-    int l2 = strlen(n2);
-    int carry = 0;
-    int max_l = l1 > l2 ? l1 : l2;
-    
-    if (max_l + 1 > size_r) {
-        return 0;
-    }
-
-    r[max_l + 1] = '\0';
-
-    while (l1 > 0 || l2 > 0) {
-        int digit1 = (l1 > 0) ? n1[--l1] - '0' : 0;
-        int digit2 = (l2 > 0) ? n2[--l2] - '0' : 0;
-        int sum = digit1 + digit2 + carry;
-        carry = sum / 10;
-        r[max_l--] = (sum % 10) + '0';
-    }
-
-    if (carry) {
-        if (max_l < 0) {
-            return 0;
-        }
-        r[max_l] = carry + '0';
-    }
-
-    return &r[max_l];
+ int i, j, k, l, m, n;
+for (i = 0; n1[i]; i++)
+;
+for (j = 0; n2[j]; j++)
+;
+if (i > size_r || j > size_r)
+return (0);
+m = 0;
+for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+{
+n = m;
+if (i >= 0)
+n += n1[i] - '0';
+if (j >= 0)
+n += n2[j] - '0';
+if (i < 0 && j < 0 && n == 0)
+{
+break;
+}
+m = n / 10;
+r[k] = n % 10 + '0';
+}
+r[k] = '\0';
+if (i >= 0 || j >= 0 || m)
+return (0);
+for (k -= 1, l = 0; l < k; k--, l++)
+{
+m = r[k];
+r[k] = r[l];
+r[l] = m;
+}
+return (r);
 }
