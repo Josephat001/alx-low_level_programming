da81469cb9748f3f75968dba83aca1ec49ff045d
Josephat Oyoke <141118204+Josephat001@users.noreply.github.com>
2023-10-20 16:15:45 +0100

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,43 +1,16 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
+
 /**
  * print_line - this function prints a buffer,
  * printing the content of 'size' bytes of the buffer pointed by 'b'
  * @b: buffer to print
  * @size: bytes of buffer to print
- * @l: line of buffer to print
+ * @line: line of buffer to print
  *
  * Return: void
  */
 
-void print_buffer(char *b, int size)
-{
-int j, k;
-for (j = 0; j <= 9; j++)
-{
-if (j <= s)
-printf("%02x", c[l * 10 + j]);
-else
-printf("  ");
-if (j % 2)
-putchar(' ');
-}
-for (k = 0; k <= s; k++)
-{
-if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-putchar(c[l * 10 + k]);
-else
-putchar('.');
-}
-}
-
-/**
- * print_buffer - prints a buffer
- * @b: buffer to print
- * @size: size of buffer
- *
- * Return: void
- */
 void print_buffer(char *b, int size)
 {
 int i;
