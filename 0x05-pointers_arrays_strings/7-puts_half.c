bdc3d70d4f629a793b8d75ba6752f89ff4be1134
Josephat Oyoke <141118204+Josephat001@users.noreply.github.com>
2023-10-22 12:06:53 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,30 +11,28 @@
 
 void puts_half(char *str)
 {
-	int i = 0;
-	int n;
-	int length_of_the_string = 0;
+    int i = 0;
+    int n;
+    int length_of_the_string = 0;
 
-	while (str[length_of_the_string] != '\0')
-	{
-		length_of_the_string++;
-	}
-	n = (length_of_the_string - 1) / 2;
+    while (str[length_of_the_string] != '\0')
+    {
+        length_of_the_string++;
+    }
 
-	if (length_of_the_string % 2 == 0)
-	{
-		for (i = n + 1; i < length_of_the_string; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
-	else
-	{
-		n = length_of_the_string / 2;
-		for (i = n; i < length_of_the_string; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
-	_putchar('\n');
+    if (length_of_the_string % 2 == 0)
+    {
+        n = length_of_the_string / 2;
+    }
+    else
+    {
+        n = (length_of_the_string - 1) / 2;
+    }
+
+    for (i = n; i < length_of_the_string; i++)
+    {
+        _putchar(str[i]);
+    }
+
+    _putchar('\n');
 }
