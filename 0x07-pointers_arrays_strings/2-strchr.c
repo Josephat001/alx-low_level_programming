057e603ae7eece7b2608c3abdc4435098a9daa8d
Josephat Oyoke <141118204+Josephat001@users.noreply.github.com>
2023-10-23 22:33:44 +0100

    Update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,14 +12,17 @@
 
 char *_strchr(char *s, char c)
 {
-
-	while (*s != '\0')
+	int i = 0;
+	
+	while (s[i] != '\0' && s[i] != c)
+	i++;
+	
+	if (s[i] == c)
 	{
-		if (*s == c)
-		{
-			return (char *)s;
-		}
-		else
-			return (NULL);
+		return (&s[i]);
+	}
+	else
+	{
+		return (NULL);
 	}
 }
