81a3c1a3de10036561f24e6d0f7255b73b09226d
josephat001 <jayoyoke@gmail.com>
2023-10-23 14:13:23 -0700

    Update

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - check 2-main.c
+ * Description: this function locates a character in a string
+ * @c: a character whose first occurence is to be located in 's'
+ * @s: pointer to the string in which character 'c' is to be copied from
+ *
+ * Return: pointer to 'c'.
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (char *)s;
+		}
+	}
+	return (NULL);
+}
