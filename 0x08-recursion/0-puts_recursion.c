bf39577a0545e38478c7cdc4b3fcf6ffebf54078
Josephat Oyoke <141118204+Josephat001@users.noreply.github.com>
2023-10-25 14:00:42 +0100

    Update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,10 +11,10 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		
-		return;
-	else
-		_putchar(*s);
-		_puts_recursion(s + 1);
+if (*s == '\0')
+
+return;
+
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
