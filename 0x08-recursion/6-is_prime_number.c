ac58d65f081e63824c2fdc5f53ec938ae418a94e
josephat001 <jayoyoke@gmail.com>
2023-10-25 11:08:36 -0700

    Update

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,29 +2,42 @@
 #include "main.h"
 
 /**
- * is_prime_number - check main.c
- * Description: this function prints prime numbers
- * @n: the number to be checked
+ * is_root - a helper function to check if 'n' is a prime number
+ * @n: the integer to be checked
+ * @a: the divisor to test
+ *
+ * Return: 1 if the input integer is a prime number, otherwise 0
+ */
+
+int is_root(int n, int a)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	if (a * a > n)
+	{
+	return (1);
+	}
+
+	if (n % 2 == '\0')
+	{
+		return (0);
+	}
+
+	return (is_root(n, a + 1));
+}
+
+/**
+ * is_prime_number - this function prints prime numbers
+ * @n: the integer to be checked
  *
  * Return: 1 if the input integer is a prime number, otherwise 0
  */
 
 int is_prime_number(int n)
 {
-	int a;
-
-	if (n <= 1)
-	{
-		return (0);
-	}
-
-	for (a = 2; a * a < n; a++)
-	{
-		if (n % a == '\0')
-		{
-			return (0);
-		}
-	}
-
-	return (1);
+	return (is_root(n, 2));
 }
+
