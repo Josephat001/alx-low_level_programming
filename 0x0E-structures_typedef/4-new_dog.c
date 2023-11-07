5791281f6e78c13b8e38e902665dd4ce02751a04
Josephat Oyoke <141118204+Josephat001@users.noreply.github.com>
2023-11-07 17:15:26 +0100

    Update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,41 +11,32 @@
  * Return: pointer to the new dog, or NULL on failure
  */
 
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-unsigned int i, namel, ownerl;
-dog_t *dog;
+    if (name == NULL || owner == NULL)
+        return (NULL);
 
-if (name == NULL || owner == NULL)
-return (NULL);
-dog = malloc(sizeof(dog_t));
-if (dog == NULL)
-return (NULL);
-for (namel = 0; name[namel]; namel++)
-;
-namel++;
-dog->name = malloc(namel * (sizeof(char)));
-if (dog->name == NULL)
-{
-free(dog);
-return (NULL);
-}
-for (i = 0; i < namel; i++)
-dog->name[i] = name[i];
+    dog_t *dog = malloc(sizeof(dog_t));
+    if (dog == NULL)
+        return (NULL);
 
-dog->age = age;
+    dog->name = strdup(name);
+    if (dog->name == NULL)
+    {
+        free(dog);
+        return (NULL);
+    }
 
-for (ownerl = 0; owner[ownerl]; ownerl++)
-;
-ownerl++;
-dog->owner = malloc(ownerl * (sizeof(char)));
-if (dog->owner == NULL)
-{
-free(dog->name);
-free(dog);
-return (NULL);
-}
-for (i = 0; i < ownerl; i++)
-dog->owner[i] = owner[i];
-return (dog);
+    dog->age = age;
+
+    dog->owner = strdup(owner);
+    if (dog->owner == NULL)
+    {
+        free(dog->name);
+        free(dog);
+        return (NULL);
+    }
+
+    return (dog);
 }
